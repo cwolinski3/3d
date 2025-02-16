b5b19c45a30abac6bfc408bcd5b7e3791697d84f
cwolinski3 <113201340+cwolinski3@users.noreply.github.com>
2025-02-15 22:39:53 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+    import React from 'react';
+
+    function App() {
+        return (
+            <div>
+                <h1>Welcome to 3 Degrees</h1>
+                <p>The game is under development.</p>
+            </div>
+        );
+    }
+
+    export default App;
+    
